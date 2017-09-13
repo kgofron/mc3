1828889ad676b21f043cd5993d341125a85ad859
kgofron <kgofron@bnl.gov>
2017-09-13 18:02:11 -0400

    spectrometer tower Y offsets

---
+++ offsetSpectromer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This bash script offsets the spectrometer tower coordinated Y motion, and sample coordinated Y motion
+
+#====================================
+#IXS Spectrometer 			
+#&3 Sample tower Y		Ect [nm]	Offset[nm]
+#Y1	P4902	20254823	230523.666666668
+#Y2	P4903	19902244	-122055.333333332
+#Y3	P4904	19915831	-108468.333333332
+			
+#Sum		60072898	
+#Average		20024299.3333333	
+#=====================================
+caput XF:10IDD-CT{MC:11}Asyn.AOUT "&3 P4902=-0.230567"
+caput XF:10IDD-CT{MC:11}Asyn.AOUT "&3 P4903=0.122133"
+caput XF:10IDD-CT{MC:11}Asyn.AOUT "&3 P4904=0.108433"
